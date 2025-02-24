ef645ccf3645ae056a8cb936f3b08299681e9d51
Vian Espost <vespost@gmail.com>
2025-02-24 15:40:54 +0200

    testing

--- html/wp-content/themes/coct-theme/page-templates/EconomicAnalysisPage.php
+++ html/wp-content/themes/coct-theme/page-templates/EconomicAnalysisPage.php
@@ -32,26 +32,6 @@ GDP GROWTH RATE, 2024 Q1
 
 R145 985
 
-<div class="w3-bar w3-black">
-  <button class="w3-bar-item w3-button" onclick="openCity('London')">London</button>
-  <button class="w3-bar-item w3-button" onclick="openCity('Paris')">Paris</button>
-  <button class="w3-bar-item w3-button" onclick="openCity('Tokyo')">Tokyo</button>
-</div>
-
-<div id="London" class="city">
-  <h2>London</h2>
-  <p>London is the capital of England.</p>
-</div>
-
-<div id="Paris" class="city" style="display:none">
-  <h2>Paris</h2>
-  <p>Paris is the capital of France.</p>
-</div>
-
-<div id="Tokyo" class="city" style="display:none">
-  <h2>Tokyo</h2>
-  <p>Tokyo is the capital of Japan.</p>
-</div>
 
 
 </div>
